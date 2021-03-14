4dd2e3a8917dcb28f0f2f7c8e9084e5baaec7268
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-15 00:25:26 +0300

    ✨ Add new component car

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarComponent } from './components/car/car.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarComponent
   ],
   imports: [
     BrowserModule,
