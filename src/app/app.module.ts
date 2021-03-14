cd0fbf13b43342c9ad8657dc21f5e3a86a03ebdd
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-15 00:27:19 +0300

    ✨ Add new component color

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
 import { BrandComponent } from './components/brand/brand.component';
+import { ColorComponent } from './components/color/color.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
-    BrandComponent
+    BrandComponent,
+    ColorComponent
   ],
   imports: [
     BrowserModule,
