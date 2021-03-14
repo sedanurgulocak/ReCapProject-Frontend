51de37bead7d0a024346186c22dab9121b4ab3a5
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-15 00:28:28 +0300

    ✨ Add new component customer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { CarComponent } from './components/car/car.component';
 import { BrandComponent } from './components/brand/brand.component';
 import { ColorComponent } from './components/color/color.component';
+import { CustomerComponent } from './components/customer/customer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CarComponent,
     BrandComponent,
-    ColorComponent
+    ColorComponent,
+    CustomerComponent
   ],
   imports: [
     BrowserModule,
