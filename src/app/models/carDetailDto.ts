d9b374d8236c7ff29200622b4b4d1cef5849d489
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-15 22:36:04 +0300

    ✨ Create Car detail dto model

---
+++ src/app/models/carDetailDto.ts
@@ -0,0 +1,9 @@
+export interface CarDetailDto{
+    id:number;
+    descriptions:string;
+    brandName:string;
+    colorName:string;
+    dailyPrice:number;
+    carImageDate:Date;
+    imagePath:string;
+}
