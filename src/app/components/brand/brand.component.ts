da0213cc1586a82dbf3e2b018e8987921ac6bc24
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-19 00:52:41 +0300

    🔨 Add new methods for brand component

--- src/app/components/brand/brand.component.ts
+++ src/app/components/brand/brand.component.ts
@@ -9,8 +9,10 @@ import { BrandService } from 'src/app/services/brand.service';
 })
 export class BrandComponent implements OnInit {
 
-  brands:Brand[]=[]
+  brands : Brand[]=[]
   dataLoaded = false;
+  currentBrand : Brand;
+  removeBrand : Brand;
 
   constructor(private brandService:BrandService) { }
 
@@ -25,4 +27,27 @@ export class BrandComponent implements OnInit {
     })
   }
 
+  setCurrentBrand(brand:Brand){
+    this.currentBrand = brand;
+  }
+
+  getCurrentBrandClass(brand:Brand){
+    if(brand === this.currentBrand){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+  }
+
+  getAllBrandClass(){
+    if(!this.currentBrand){
+      return "list-group-item active"
+    }else{
+      return "list-group-item"
+    }
+  }
+
+  removeCurrentBrand(){
+    this.currentBrand=this.removeBrand;
+  }
 }
