d3deafd0c60e5757ebbc9406226b06c7846ada15
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-19 02:19:43 +0300

    🔨 Add operations to CarImage service

--- src/app/services/car-image.service.ts
+++ src/app/services/car-image.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CarImage } from '../models/carImage';
+import { ListResponseModel } from '../models/listResponseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CarImageService {
+  
+  apiUrl = "https://localhost:44303/api/carImages/getall"
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+  getCarImages():Observable<ListResponseModel<CarImage>>{
+    return this.httpClient.get<ListResponseModel<CarImage>>(this.apiUrl);
+  }
 }
