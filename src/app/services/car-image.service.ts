c926c6abd90b4224f813de8c78b38808815f2ed5
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-21 19:00:08 +0300

    🔨 Add new method getCarImages and update apiUrl

--- src/app/services/car-image.service.ts
+++ src/app/services/car-image.service.ts
@@ -9,11 +9,17 @@ import { ListResponseModel } from '../models/listResponseModel';
 })
 export class CarImageService {
   
-  apiUrl = "https://localhost:44303/api/carImages/getall"
+  apiUrl = "https://localhost:44303/api/carImages/"
 
   constructor(private httpClient:HttpClient) { }
 
   getCarImages():Observable<ListResponseModel<CarImage>>{
-    return this.httpClient.get<ListResponseModel<CarImage>>(this.apiUrl);
+    let newPath=this.apiUrl+"getall"
+    return this.httpClient.get<ListResponseModel<CarImage>>(newPath);
+  }
+
+  getCarImagesByCarId(carId:number):Observable<ListResponseModel<CarImage>>{
+    let newPath=this.apiUrl+"getbycarid?carId="+carId
+    return this.httpClient.get<ListResponseModel<CarImage>>(newPath)
   }
 }
