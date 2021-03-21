6ff1233c3c7458ffe6225d42588e5a912a6c7b0f
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-21 19:01:51 +0300

    🔨 Add new method getSingleCar

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -28,8 +28,13 @@ export class CarService {
     return this.httpClient.get<ListResponseModel<CarDetailDto>>(newPath);
   }
   
-  getCarsByCarId(carId:number):Observable<ListResponseModel<CarDetailDto>>{
-    let newPath = this.apiUrl + "cars/getcardetaildtos?id="+carId
+  getCarByCarId(carId:number):Observable<ListResponseModel<CarDetailDto>>{
+    let newPath = this.apiUrl + "cars/getcardetaildtobyid?id="+carId
+    return this.httpClient.get<ListResponseModel<CarDetailDto>>(newPath);
+  }
+
+  getSingleCar(carId:number):Observable<ListResponseModel<CarDetailDto>>{
+    let newPath = this.apiUrl + "cars/"+carId
     return this.httpClient.get<ListResponseModel<CarDetailDto>>(newPath);
   }
 }
