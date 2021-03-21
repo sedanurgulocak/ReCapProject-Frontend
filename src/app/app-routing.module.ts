f2f1f365b1ad2e6240d52e751f3084a51871e18b
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-21 18:42:12 +0300

    🔨 Add new route for CarDetailComponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
   {path:"cars", component:CarComponent},
   {path:"cars/brand/:brandId", component:CarComponent},
   {path:"cars/color/:colorId", component:CarComponent},
-  {path:"cars/getcardetaildtos/:carId", component:CarDetailComponent}
+  {path:"cars/getcardetail/:carId", component:CarDetailComponent}
 
 
 ];
