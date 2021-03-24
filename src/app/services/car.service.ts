b3b45f194c3f8a798c95dcf4558b1517037d0586
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-24 15:47:52 +0300

    🔨 Add new method for filter to car service

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -37,4 +37,9 @@ export class CarService {
     let newPath = this.apiUrl + "cars/"+carId
     return this.httpClient.get<ListResponseModel<CarDetailDto>>(newPath);
   }
+
+  getCarsByBrandIdAndColorId(brandId:number, colorId:number):Observable<ListResponseModel<CarDetailDto>>{
+    let newPath = this.apiUrl + "cars/getcarsbybrandidandcolorid?brandId="+brandId+"&colorId="+colorId
+    return this.httpClient.get<ListResponseModel<CarDetailDto>>(newPath);
+  }
 }
