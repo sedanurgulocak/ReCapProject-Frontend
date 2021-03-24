a1fab1cbd1c4a971d007596b0d793465e3092b4e
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-24 16:09:39 +0300

    🔨 Add new path for rental component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarDetailComponent } from './components/car-detail/car-detail.component';
 import { CarComponent } from './components/car/car.component';
+import { RentalComponent } from './components/rental/rental.component';
 
 const routes: Routes = [
   {path:"",pathMatch:"full", component:CarComponent},
@@ -9,7 +10,8 @@ const routes: Routes = [
   {path:"cars/brand/:brandId", component:CarComponent},
   {path:"cars/color/:colorId", component:CarComponent},
   {path:"cars/getcardetail/:carId", component:CarDetailComponent},
-  {path:"cars/filter/:brandId/:colorId", component:CarComponent}
+  {path:"cars/filter/:brandId/:colorId", component:CarComponent},
+  {path:"cars/rental/:id", component:RentalComponent}
 
 
 ];
