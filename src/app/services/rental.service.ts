1408be179911db73fdfbae0d8f8078324913f929
sedanurgulocak <sedanurgulocak@gmail.com>
2021-03-28 21:22:16 +0300

    🔨 Add add method to rental service

--- src/app/services/rental.service.ts
+++ src/app/services/rental.service.ts
@@ -2,18 +2,31 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ListResponseModel } from '../models/listResponseModel';
+import { Rental } from '../models/rental';
 import { RentalDetailDto } from '../models/rentalDetailDto';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class RentalService {
 
-  apiUrl = "https://localhost:44303/api/rentals/getrentaldetaildtos"
+  apiUrl = "https://localhost:44303/api/rentals"
 
   constructor(private httpClient:HttpClient) { }
 
-  getRentals():Observable<ListResponseModel<RentalDetailDto>>{
-    return this.httpClient.get<ListResponseModel<RentalDetailDto>>(this.apiUrl);
+  add(rental:Rental, carId:number):Observable<ResponseModel>{
+    rental.carId = carId;
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"/add", rental);
   }
+
+  // getRentals():Observable<ListResponseModel<RentalDetailDto>>{
+  //   let newPath = this.apiUrl + "/getrentaldetaildtos"
+  //   return this.httpClient.get<ListResponseModel<RentalDetailDto>>(newPath);
+  // }
+
+  // getRentalDetailByCarId(carId : number):Observable<ListResponseModel<RentalDetailDto>>{
+  //   let newPath = this.apiUrl + "/getrentaldetailsbyid?id=" + carId
+  //   return this.httpClient.get<ListResponseModel<RentalDetailDto>>(newPath);
+  // }
 }
