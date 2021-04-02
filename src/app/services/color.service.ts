0407d5b3dfa25008a91356f23b9a238b56556880
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-03 01:59:57 +0300

    🔨 Add new method for color add

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -3,17 +3,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Color } from '../models/color';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ColorService {
 
-  apiUrl = "https://localhost:44303/api/colors/getall"
+  apiUrl = "https://localhost:44303/api"
 
   constructor(private httpClient:HttpClient) { }
 
   getColors():Observable<ListResponseModel<Color>>{
-    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl);
+    return this.httpClient.get<ListResponseModel<Color>>(this.apiUrl+"/colors/getall");
+  }
+
+  add(color:Color):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"/colors/add", color);
   }
 }
