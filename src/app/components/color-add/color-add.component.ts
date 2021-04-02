8316aca5aae25b4cc9058c44bedefd7a8adb6583
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-03 02:00:27 +0300

    🔨 Add color add operations

--- src/app/components/color-add/color-add.component.ts
+++ src/app/components/color-add/color-add.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { ColorService } from 'src/app/services/color.service';
 
 @Component({
   selector: 'app-color-add',
@@ -7,9 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorAddComponent implements OnInit {
 
-  constructor() { }
+  colorAddForm :FormGroup;
+  constructor(private formBuilder:FormBuilder, private colorService:ColorService, private toastrService:ToastrService) { }
 
   ngOnInit(): void {
+    this.createColorAddForm();
+  }
+
+  createColorAddForm(){
+    this.colorAddForm = this.formBuilder.group({
+      colorName:["", Validators.required]
+    })
+  }
+
+  add(){
+    if(this.colorAddForm.valid){
+      let colorModel = Object.assign({}, this.colorAddForm.value)
+      this.colorService.add(colorModel).subscribe(response => {
+        this.toastrService.success(response.message, "Successful")
+      }, responseError =>{
+        if(responseError.error.ValidationError.length>0){
+          for (let i = 0; i < responseError.error.ValidationError.length; i++) {
+            this.toastrService.error(responseError.error.ValidationError[i].ErrorMessage, "ValidationError")
+          }
+        }
+      })
+    }else{
+      this.toastrService.error("Your form is incopmlete");
+    }
   }
 
 }
