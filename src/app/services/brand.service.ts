fedab5c598ee9de52528757d6d8581e3cc645394
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-03 01:35:49 +0300

    🔨 Add new method for brand add

--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -3,17 +3,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Brand } from '../models/brand';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BrandService {
 
-  apiUrl = "https://localhost:44303/api/brands/getall"
+  apiUrl = "https://localhost:44303/api"
 
   constructor(private httpClient:HttpClient) { }
 
   getBrands():Observable<ListResponseModel<Brand>>{
-    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl);
+    return this.httpClient.get<ListResponseModel<Brand>>(this.apiUrl+"/brands/getall");
+  }
+
+  add(brand:Brand):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"/brands/add", brand);
   }
 }
