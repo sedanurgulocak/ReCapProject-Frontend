51d04093f28f2c3c509c61fc080478926796b7f4
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-03 00:56:08 +0300

    🔨 Add new method for car add

--- src/app/services/car.service.ts
+++ src/app/services/car.service.ts
@@ -1,8 +1,10 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Car } from '../models/car';
 import { CarDetailDto } from '../models/carDetailDto';
 import { ListResponseModel } from '../models/listResponseModel';
+import { ResponseModel } from '../models/responseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -41,5 +43,9 @@ export class CarService {
   getCarsByBrandIdAndColorId(brandId:number, colorId:number):Observable<ListResponseModel<CarDetailDto>>{
     let newPath = this.apiUrl + "cars/getcarsbybrandidandcolorid?brandId="+brandId+"&colorId="+colorId
     return this.httpClient.get<ListResponseModel<CarDetailDto>>(newPath);
+  }
+
+  add(car:Car):Observable<ResponseModel>{
+    return this.httpClient.post<ResponseModel>(this.apiUrl+"cars/add", car);
   }
 }
