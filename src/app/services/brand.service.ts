6f9df375f9c3e6593ccade20784f03c2adc0f297
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-03 16:02:34 +0300

    🔨 Add new method for get brand by id

--- src/app/services/brand.service.ts
+++ src/app/services/brand.service.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { Brand } from '../models/brand';
 import { ListResponseModel } from '../models/listResponseModel';
 import { ResponseModel } from '../models/responseModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,10 @@ export class BrandService {
 
   add(brand:Brand):Observable<ResponseModel>{
     return this.httpClient.post<ResponseModel>(this.apiUrl+"/brands/add", brand);
+  }
+
+  getSingleBrand(brandId:number):Observable<SingleResponseModel<Brand>>{
+    let newPath = this.apiUrl + "/brands/getbyid?id=" + brandId
+    return this.httpClient.get<SingleResponseModel<Brand>>(newPath);
   }
 }
