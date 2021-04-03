1d0d7773e1bd9dbe5dad03710162c6031600a6de
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-03 19:09:38 +0300

    🔨 Add color update operations

--- src/app/components/color-update/color-update.component.ts
+++ src/app/components/color-update/color-update.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validator, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Color } from 'src/app/models/color';
+import { ColorService } from 'src/app/services/color.service';
 
 @Component({
   selector: 'app-color-update',
@@ -7,9 +12,50 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ColorUpdateComponent implements OnInit {
 
-  constructor() { }
+  color:Color;
+  path:string="https://localhost:44303";
 
-  ngOnInit(): void {
+  colorUpdateForm : FormGroup
+
+  constructor(private colorService:ColorService, private activatedRoute:ActivatedRoute, private formBuilder:FormBuilder, private toastrService:ToastrService) { }
+
+  ngOnInit(): void {this.activatedRoute.params.subscribe(params => {
+    if(params["colorId"]){
+      this.getColorDetail(params["colorId"])
+    }
+  })
+    this.createColorUpdateForm();
   }
 
+  getColorDetail(colorId:number){
+    this.colorService.getSingleColor(colorId).subscribe(response => {
+      this.color = response.data;
+      console.log(response);
+    })
+  }
+
+  createColorUpdateForm(){
+    this.colorUpdateForm = this.formBuilder.group({
+      colorId:["", Validators.required],
+      colorName:["", Validators.required]
+    })
+  }
+
+  update(){
+    if(this.colorUpdateForm.valid){
+      let colorModel = Object.assign({}, this.colorUpdateForm.value)
+      this.colorService.update(colorModel).subscribe(response => {
+        this.toastrService.success(response.message, "Successful")
+      }, responseError => {
+        if(responseError.error.ValidationErrors.length>0){
+          for (let i = 0; i < responseError.error.ValidationErrors.length; i++) {
+            this.toastrService.error(responseError.error.ValidationErrors[i].ErrorMessage, "Validation Error")
+            
+          }
+        }
+      })
+    }else{
+      this.toastrService.error("Your form is incomplete");
+    }
+  }
 }
