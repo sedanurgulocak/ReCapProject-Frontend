a45497f22b0343126506a9f66a21a1795057ee48
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-03 19:09:20 +0300

    🔨 Add new methods get single color and update

--- src/app/services/color.service.ts
+++ src/app/services/color.service.ts
@@ -1,9 +1,10 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { Injectable, OnChanges } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Color } from '../models/color';
 import { ListResponseModel } from '../models/listResponseModel';
 import { ResponseModel } from '../models/responseModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
 
 @Injectable({
   providedIn: 'root'
@@ -20,5 +21,14 @@ export class ColorService {
 
   add(color:Color):Observable<ResponseModel>{
     return this.httpClient.post<ResponseModel>(this.apiUrl+"/colors/add", color);
+  }
+
+  getSingleColor(colorId:number):Observable<SingleResponseModel<Color>>{
+    let newPath = this.apiUrl+ "/colors/getbyid?id=" + colorId
+    return this.httpClient.get<SingleResponseModel<Color>>(newPath);
+  }
+
+  update(color:Color):Observable<SingleResponseModel<Color>>{
+    return this.httpClient.post<SingleResponseModel<Color>>(this.apiUrl+ "/colors/update", color);
   }
 }
