7f1bf2b3a45b53dc5bd3a6e63cd25797d1d62c3a
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-03 03:41:09 +0300

    🔨 Add new path car list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { BrandAddComponent } from './components/brand-add/brand-add.component';
 import { BrandListComponent } from './components/brand-list/brand-list.component';
 import { CarAddComponent } from './components/car-add/car-add.component';
 import { CarDetailComponent } from './components/car-detail/car-detail.component';
+import { CarListComponent } from './components/car-list/car-list.component';
 import { CarComponent } from './components/car/car.component';
 import { ColorAddComponent } from './components/color-add/color-add.component';
 import { ColorListComponent } from './components/color-list/color-list.component';
@@ -21,7 +22,8 @@ const routes: Routes = [
   {path:"brands/add", component:BrandAddComponent},
   {path:"colors/add", component:ColorAddComponent},
   {path:"brands/getall", component:BrandListComponent},
-  {path:"colors/getall", component:ColorListComponent}
+  {path:"colors/getall", component:ColorListComponent},
+  {path:"cars/getall", component:CarListComponent}
 
 
 ];
