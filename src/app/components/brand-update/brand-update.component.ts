556d8d27e38d12d22a022ac80486a60e9c3f57ab
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-03 18:30:20 +0300

    🔨 Add brand update operations

--- src/app/components/brand-update/brand-update.component.ts
+++ src/app/components/brand-update/brand-update.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { Brand } from 'src/app/models/brand';
+import { BrandService } from 'src/app/services/brand.service';
 
 @Component({
   selector: 'app-brand-update',
@@ -7,9 +12,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BrandUpdateComponent implements OnInit {
 
-  constructor() { }
+  brand:Brand;
+  path:string="https://localhost:44303";
+  brandId:number;
+  brandName:string;
 
-  ngOnInit(): void {
+  brandUpdateForm : FormGroup;
+
+  constructor(private brandService:BrandService, private activatedRoute:ActivatedRoute, private formBuilder:FormBuilder, private toastrService:ToastrService) { }
+
+  ngOnInit(): void {this.activatedRoute.params.subscribe(params => {
+    if(params["brandId"]){
+      this.getBrandDetail(params["brandId"])
+    }
+  })
+  this.createBrandUpdateForm();
+
+  }
+
+  getBrandDetail(brandId:number){
+    this.brandService.getSingleBrand(brandId).subscribe(response => {
+      this.brand = response.data;
+      console.log(response);
+    })
+  }
+
+  createBrandUpdateForm(){
+    this.brandUpdateForm = this.formBuilder.group({
+      brandId:["", Validators.required],
+      brandName:["", Validators.required]
+    })
+  }
+
+  update(){
+    if(this.brandUpdateForm.valid){
+      let brandModel = Object.assign({}, this.brandUpdateForm.value)
+      this.brandService.update(brandModel).subscribe(response => {
+        this.toastrService.success(response.message, "Successful")
+      }, responseError => {
+        if(responseError.error.ValidationErrors.length>0){
+          for (let i = 0; i < responseError.error.ValidationErrors.length; i++) {
+            this.toastrService.error(responseError.error.ValidationErrors[i].ErrorMessage, "Validation Error")
+            
+          }
+        }
+      })
+    }else{
+      this.toastrService.error("Your form is incomplete");
+    }
   }
 
 }
