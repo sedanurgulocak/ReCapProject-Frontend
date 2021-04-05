26c18c1138d70b5ec33a537060baf06214157ec0
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-05 20:04:29 +0300

    ✨ Add auth service

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,29 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginModel } from '../models/loginModel';
+import { SingleResponseModel } from '../models/singleResponseModel';
+import { TokenModel } from '../models/tokenModel';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  apiUrl = 'https://localhost:44303/api/auth/'
+
+  constructor(private httpClient:HttpClient) { }
+
+  login(loginModel:LoginModel):Observable<SingleResponseModel<TokenModel>>{
+    return this.httpClient.post<SingleResponseModel<TokenModel>>(this.apiUrl+"login", loginModel);
+  }
+
+  isAuthenticated(){
+    if(localStorage.getItem("token")){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+}
