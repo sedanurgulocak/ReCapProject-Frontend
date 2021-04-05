1cd604c29fdb80aabb3761ee69012bee39f7d79d
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-05 21:42:50 +0300

    🔨 Add register operations

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  registerForm:FormGroup;
+
+  constructor(private formBuilder:FormBuilder, private authService:AuthService, private toastrService:ToastrService, private router:Router) { }
 
   ngOnInit(): void {
+    this.createRegisterForm();
   }
 
+  createRegisterForm(){
+    this.registerForm = this.formBuilder.group({
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      email: ["", Validators.required],
+      password: ["", Validators.required]
+    })
+  }
+
+  register(){
+    if(this.registerForm.valid){
+      let registerModel = Object.assign({}, this.registerForm.value)
+      this.authService.register(registerModel).subscribe(response => {
+        this.toastrService.success(response.message, "Successful")
+      }, responseError => {
+        if(responseError.error.ValidationError.length>0){
+          for(let i = 0; i < responseError.error.ValidationErrors.length; i++){
+            this.toastrService.error(responseError.error.ValidationErrors[i].ErrorMessage, "ValidationError")
+          }
+        }
+      })
+    }else{
+      this.toastrService.error("Your form is incomplete");
+    }
+  }
 }
