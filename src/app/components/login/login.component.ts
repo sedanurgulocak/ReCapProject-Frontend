9f1339bdb836218e941b96d1d6043284e6566676
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-09 01:22:19 +0300

    🔨 Refactor according to local storage

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormGroup, FormControl, Validators, FormBuilder } from "@angular/forms";
 import { ToastrService } from 'ngx-toastr';
 import { AuthService } from 'src/app/services/auth.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent implements OnInit {
 
   loginForm:FormGroup;
 
-  constructor(private formBuilder:FormBuilder, private authService:AuthService, private toastrService:ToastrService) { }
+  constructor(private formBuilder:FormBuilder, private authService:AuthService, private toastrService:ToastrService, private localStorageService:LocalStorageService) { }
 
   ngOnInit(): void {
     this.createLoginForm();
@@ -31,7 +32,9 @@ export class LoginComponent implements OnInit {
       let loginModel = Object.assign({}, this.loginForm.value)
       this.authService.login(loginModel).subscribe(response => {
         this.toastrService.info(response.message)
-        localStorage.setItem("token", response.data.token)
+        this.localStorageService.set("token", response.data.token);
+        this.localStorageService.set("email", this.loginForm.value.email);
+        
       }, responseError =>{
         this.toastrService.error(responseError.error)
       })
