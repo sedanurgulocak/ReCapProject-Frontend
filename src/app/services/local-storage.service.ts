c8032662aaea121b988c2f2181dce86e66ed943a
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-09 01:20:29 +0300

    ✨ Add new service, local storage service

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocalStorageService {
+
+  localStorage:Storage;
+  constructor() { 
+    this.localStorage = window.localStorage;
+  }
+
+  get(key : string){
+    return this.localStorage.getItem(key);
+  }
+
+  set(key: string, value: string){
+    this.localStorage.setItem(key,value);
+  }
+
+  remove(key: string){
+    this.localStorage.removeItem(key);
+  }
+  
+
+  clean(){
+    localStorage.clear();
+  }
+}
