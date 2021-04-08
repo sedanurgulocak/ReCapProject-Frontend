b67dc32d917b6dd204d1957ef850439cbe5c7d94
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-09 01:25:46 +0300

    🔨 Add user component operations

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/models/user';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-user',
@@ -7,9 +12,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserComponent implements OnInit {
 
-  constructor() { }
+  userUpdateForm:FormGroup;
+  email = this.localStorageService.get("email");
+  password:FormControl;
+  user: User = new User();
+
+  constructor(private userService:UserService, private formBuilder:FormBuilder, private toastrService:ToastrService, private localStorageService:LocalStorageService) { }
 
   ngOnInit(): void {
+    this.createUserUpdateForm();
+    this.getUser();
+  }
+
+  createUserUpdateForm(){
+    this.userUpdateForm = this.formBuilder.group({
+      userId:this.user.userId,
+      firstName:["", Validators.required],
+      lastName:["", Validators.required],
+      email:["", Validators.required],
+      password:["", Validators.required]
+
+    })
+  }
+
+  getUser(){
+    if(this.email){
+      this.userService.getByEmail(this.email).subscribe(response => {
+        this.user = response;
+      })
+    }
+  }
+
+  update(){
+    if(this.userUpdateForm.valid){
+      let userModel = Object.assign({}, this.userUpdateForm.value)
+      this.userService.update(userModel).subscribe(response => {
+        this.toastrService.success(response.message, "Successful")
+      }, responseError => {
+        if(responseError.error.ValidationError.length>0){
+          for (let i = 0; i < responseError.error.ValidationErrors.length; i++){
+            this.toastrService.error(responseError.error.ValidationErrors[i].ErrorMessage, "Validation Error")
+          }
+        }
+      })
+    }else{
+      this.toastrService.error("Your form is incomplete");
+    }
   }
 
 }
