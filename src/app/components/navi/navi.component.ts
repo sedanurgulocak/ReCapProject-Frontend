f5f47c9d428061c908ffb0907a967d4cf107e3f5
sedanurgulocak <sedanurgulocak@gmail.com>
2021-04-09 01:43:30 +0300

    🔨 Update navi component for login and register

--- src/app/components/navi/navi.component.ts
+++ src/app/components/navi/navi.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { User } from 'src/app/models/user';
+import { AuthService } from 'src/app/services/auth.service';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-navi',
@@ -7,9 +13,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NaviComponent implements OnInit {
 
-  constructor() { }
+  email = this.localStorageService.get("email");
+  user:User = new User() ;
+
+  constructor(private authService:AuthService, private localStorageService:LocalStorageService, private toastrService:ToastrService, private router:Router, private userService:UserService) { }
 
   ngOnInit(): void {
+    this.checkToLogin();
+    this.checkToEmail();
+    this.getEmail();
+  }
+
+  checkToLogin(){
+    if(this.authService.isAuthenticated()){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  checkToEmail(){
+    if(this.localStorageService.get("email")){
+      return true;
+    }else{
+      return false;
+    }
+  }
+
+  getEmail(){
+    if(this.email){
+      this.userService.getByEmail(this.email).subscribe(response =>{
+        this.user = response;
+      })
+    }
+  }
+
+  logOut(){
+    this.localStorageService.clean()
+    this.toastrService.success("Logout")
+    this.router.navigate(["/cars"])
   }
 
 }
